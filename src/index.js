2472192dc83fb35584e2dc512efb0c85752d7e1c
Kris <kristian.claire.101@gmail.com>
2019-11-06 10:01:42 -0800

    updates styles and routes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'; 
-import { reducer } from './reducers/index.js';
+import { reducer } from './reducers/index';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
